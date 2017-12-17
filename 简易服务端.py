52b35f94fbff7beb6155519e22245b5625bfb89d
amazingzj <amazingzj@qq.com>
2017-12-18 00:07:48 +0800

    Add files via upload

---
+++ 简易服务端.py
@@ -0,0 +1,17 @@
+
+import socket
+
+
+def server():
+    ser=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    ser.bind(('127.0.0.1',8000))
+    ser.listen(5)
+    while 1:
+        client,addr=ser.accept()
+        print ('accept %s connect'%(addr,))
+        data=client.recv(1024)
+        print (data)
+        client.send('get')
+        client.close()
+
+server()
